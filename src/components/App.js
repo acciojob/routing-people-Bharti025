2a4a08880f999c7250c22cd19c12ec9e9e32d923
bharti025 <20001003032bharti@dcrustm.org>
2025-10-05 12:29:49 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
 import React from "react";
 import './../styles/App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import UserList from "./UserList";
+import UserDetail from "./UserDetail";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+    <Router>
+      <Routes>
+        <Route path="/" element={<UserList/>}/>
+         <Route path="/users/:id" element={<UserDetail/>}/>
+      </Routes>
+      </Router>   
+   
     </div>
   )
 }
---
+++ src/components/UserDetail.js
@@ -0,0 +1,27 @@
+import React,{useEffect,useState} from 'react'
+import {useParams} from 'react-router-dom';
+const UserDetail = () => {
+    const {id}=useParams();
+    const [users,setUsers]=useState(null);
+    useEffect(()=>{
+        fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then(response => response.json())
+        .then(data => setUsers(data))
+    },[id]);
+
+      if (!users) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <div>
+      <h1>User Details</h1>
+       <p>Name: {users.name}</p>
+        <p>Username: {users.username}</p>
+         <p>Email: {users.email}</p>
+           <p>Phone: {users.phone}</p>
+             <p>Website: {users.website}</p>
+         </div>
+  )
+}
+
+export default UserDetail
